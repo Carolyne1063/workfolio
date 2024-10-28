cc7edc5deff6d3c3058fabcba4034e210f710ec1
Carolyne1063 <carolmartoz763@gmail.com>
2024-10-27 18:06:03 -0700

    added addEmployeeForm

--- frontend/workfolio/src/app/components/hr-dashboard/employees/employees.component.ts
+++ frontend/workfolio/src/app/components/hr-dashboard/employees/employees.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css'
 })
 export class EmployeesComponent {
+  showForm = false; // Initially hidden
 
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
 }
