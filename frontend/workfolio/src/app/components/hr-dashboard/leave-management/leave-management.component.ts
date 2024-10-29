096ea77e105d8bc379b87ba9386bf43715ac4920
Carolyne1063 <carolmartoz763@gmail.com>
2024-10-28 21:24:09 -0700

    added some code

--- frontend/workfolio/src/app/components/hr-dashboard/leave-management/leave-management.component.ts
+++ frontend/workfolio/src/app/components/hr-dashboard/leave-management/leave-management.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-leave-management',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './leave-management.component.html',
   styleUrl: './leave-management.component.css'
 })
 export class LeaveManagementComponent {
-
+  leaves = [
+    {
+      employeeName: 'John Doe',
+      leaveType: 'Medical Leave',
+      from: new Date(2024, 7, 26),
+      to: new Date(2024, 8, 1),
+      noOfDays: 7,
+      reason: 'Sick',
+      status: 'Approved'
+    },
+    {
+      employeeName: 'Jane Doe',
+      leaveType: 'Medical Leave',
+      from: new Date(2024, 7, 26),
+      to: new Date(2024, 8, 1),
+      noOfDays: 7,
+      reason: 'Sick',
+      status: 'Declined'
+    },
+    {
+      employeeName: 'Sarah Smith',
+      leaveType: 'Medical Leave',
+      from: new Date(2024, 7, 26),
+      to: new Date(2024, 8, 1),
+      noOfDays: 7,
+      reason: 'Sick',
+      status: 'Pending'
+    }
+  ];
 }
